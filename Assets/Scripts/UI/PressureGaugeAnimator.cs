27e29f3f4bae6ed68a0c2093bbe1a4b1fda2549a
Jacob-h-v <jvillu21@student.aau.dk>
2026-05-04 11:22:46 +0200

    maybe fixed gauge going wild

--- Assets/Scripts/UI/PressureGaugeAnimator.cs
+++ Assets/Scripts/UI/PressureGaugeAnimator.cs
@@ -24,47 +24,60 @@ public class PressureGaugeAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // UpdateCurrentAndMaxMVC();
+        // float zRotation = Mathf.Clamp(GetGaugeRotation(), -135f, 135f);
+        // needleTransform.eulerAngles = new Vector3(0f, 0f, zRotation);
         UpdateCurrentAndMaxMVC();
-        needleTransform.eulerAngles = new Vector3(0,0,GetGaugeRotation());
+
+        float safeMvc = Mathf.Max(mvc, 0.0001f);
+        float mvcNormalized = currentPercentMVC / safeMvc;
+
+        float zRotation = Mathf.Clamp(
+            zeroMVCAngle - mvcNormalized * (zeroMVCAngle - maxMVCAngle),
+            -135f,
+            135f
+        );
+
+        needleTransform.localRotation = Quaternion.Euler(0f, 0f, zRotation);
     }
 
-    void LateUpdate()
-    {
-        //transform.position = handModel.TransformPoint(localOffset); // Offset the gauge slightly from the hand model
-        //transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position); // Make sure the gauge always faces the viewport
+    // void LateUpdate()
+    // {
+    //     //transform.position = handModel.TransformPoint(localOffset); // Offset the gauge slightly from the hand model
+    //     //transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position); // Make sure the gauge always faces the viewport
 
-        // More advance approach, hopefully it worky
-        Transform cam = Camera.main.transform;
+    //     // More advance approach, hopefully it worky
+    //     Transform cam = Camera.main.transform;
 
-        Vector3 basePos = handModel.TransformPoint(localOffset);
-        Vector3 dir = basePos-cam.position;
+    //     Vector3 basePos = handModel.TransformPoint(localOffset);
+    //     Vector3 dir = basePos-cam.position;
 
-        if(Physics.Raycast(cam.position, dir.normalized, out RaycastHit hit, dir.magnitude))
-        {
-            if(hit.transform != transform)
-            {
-                basePos = hit.point - dir.normalized * 0.03f;
-            }
-        }
+    //     if(Physics.Raycast(cam.position, dir.normalized, out RaycastHit hit, dir.magnitude))
+    //     {
+    //         if(hit.transform != transform)
+    //         {
+    //             basePos = hit.point - dir.normalized * 0.03f;
+    //         }
+    //     }
 
-        //transform.position = basePos;
-        transform.position = Vector3.Lerp(transform.position, basePos, Time.deltaTime * smoothingSpeed); // smoother motion than the line above
+    //     //transform.position = basePos;
+    //     transform.position = Vector3.Lerp(transform.position, basePos, Time.deltaTime * smoothingSpeed); // smoother motion than the line above
 
-        Vector3 flatDir = transform.position - cam.position;
-        flatDir.y = 0;
-        transform.rotation = Quaternion.LookRotation(flatDir);
+    //     Vector3 flatDir = transform.position - cam.position;
+    //     flatDir.y = 0;
+    //     transform.rotation = Quaternion.LookRotation(flatDir);
 
-        float vDot = Vector3.Dot(handModel.forward, cam.forward);
+    //     float vDot = Vector3.Dot(handModel.forward, cam.forward);
 
-        if(vDot < 0.3f)
-        {
-            localOffset.z = 0.25f; // move gauge towards camera if the hand is facing away
-        }
-        else
-        {
-            localOffset.z = 0.10f;
-        }
-    }
+    //     if(vDot < 0.3f)
+    //     {
+    //         localOffset.z = 0.25f; // move gauge towards camera if the hand is facing away
+    //     }
+    //     else
+    //     {
+    //         localOffset.z = 0.10f;
+    //     }
+    // }
 
     private void UpdateCurrentAndMaxMVC() // Fetches an update for the current and highest measured MVC
     {
@@ -75,11 +88,11 @@ public class PressureGaugeAnimator : MonoBehaviour
         currentPercentMVC = currentMVCPercent;
     }
 
-    private float GetGaugeRotation() // Calculates the needle's z-axis rotation
-    {
-        float maxAngle = zeroMVCAngle - maxMVCAngle;
-        float mvcNormalized = currentPercentMVC / mvc;
-        return zeroMVCAngle - mvcNormalized * maxAngle;
-    }
+    // private float GetGaugeRotation() // Calculates the needle's z-axis rotation
+    // {
+    //     float maxAngle = zeroMVCAngle - maxMVCAngle;
+    //     float mvcNormalized = currentPercentMVC / mvc;
+    //     return zeroMVCAngle - mvcNormalized * maxAngle;
+    // }
 
 }
