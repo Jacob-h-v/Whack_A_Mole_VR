5d72b1161c997906fe514be9550f06f4125cabaa
Jacob-h-v <jhvillumsen93@gmail.com>
2026-06-03 15:01:55 +0200

    minor precaution update for bottle breaker

--- Assets/ForceBottleBreakage.cs
+++ Assets/ForceBottleBreakage.cs
@@ -6,6 +6,12 @@ public class ForceBottleBreakage : MonoBehaviour
 {
     private bool forceBreakage = false;
 
+    void Awake()
+    {
+        // Ensure the bottle is not broken at the start
+        forceBreakage = false;
+    }
+
     public void ForceBreakage(bool value)
     {
         forceBreakage = value;
